57b97596d5c8af18771b7b1611949b3661ec1f23
hmccl <heliomccl+github@gmail.com>
2025-11-28 23:18:16 -0300

    feat: Adiciona rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import TableClient from './TableClient.tsx'
+import { Routes, Route } from 'react-router';
+import ClientList from './pages/ClientList';
+import AddClient from './pages/AddClient';
+import ClientDetails from './pages/ClientDetails';
+import AddPet from './pages/AddPet';
 
 function App() {
   return (
-    <>
-      <TableClient />
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<ClientList />} />
+      <Route path="/add-client" element={<AddClient />} />
+      <Route path="/client/:id" element={<ClientDetails />} />
+      <Route path="/client/:id/add-pet" element={<AddPet />} />
+    </Routes>
+  );
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router';
 import './index.css'
 import App from './App.tsx'
+import { ClientProvider } from './contexts/ClientContext'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <ClientProvider>
+        <App />
+      </ClientProvider>
     </BrowserRouter>
   </StrictMode>,
 )
